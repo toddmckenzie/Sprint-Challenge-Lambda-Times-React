c2631b2397fd1cbb034461bced4bf4fc25567f4e
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-05-03 12:51:29 -0500

    styled components completely finished

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -22,6 +22,14 @@ const Container = styled.div`
   color: #fff;
   letter-spacing: 1px;
   padding: 0 10px;
+
+  @media (min-width: 1280px) {
+    width: 1280px;
+  }
+
+  @media (min-width: 1280px){
+    width: 1280px;
+  }
 `;
 
 const ContainerLeft = styled.div`
@@ -49,6 +57,12 @@ const ContainerCenter = styled.div`
 const ContainerCenterSpan = styled.span`
   cursor: pointer;
   margin-right: 5%;
+  &:last-child {
+    margin-right: 0;
+  }
+  &:hover {
+    text-decoration: underline;
+  }
 `;
 
 const ConatinerRight = styled.div`
