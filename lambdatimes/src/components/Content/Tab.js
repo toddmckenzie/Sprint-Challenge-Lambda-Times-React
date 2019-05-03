611698adae342383821ffbd8fa0d4c259d27b194
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-05-03 10:56:05 -0500

    crashing inifinite loop

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
 const Tab = props => {
-  /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
-      if they match, the className should be: 'tab active-tab', 
+  /* Using your props, determine if the `tab` prop matches the `selectedTab` prop,
+      if they match, the className should be: 'tab active-tab',
       if it is not it should just be 'tab'*/
+
+      let classes = 'tab';
+    //  if (props.tab === props.selectedTab) {
+      //  classes='tab active-tab';
+      console.log(props.tab + ' here i am')
   return (
     <div
-      className={''}
-      onClick={() => {
-        /* Replace this dummy click handler function with your selectTabHandler function from props 
+      className={classes}
+      onClick={props.selectTabHandler(props.tab)
+        /* Replace this dummy click handler function with your selectTabHandler function from props
          you'll need to pass the `tab` in as an argument to this handler. */
-      }}
+      }
     >
       {props.tab.toUpperCase()}
     </div>
